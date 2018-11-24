382b14b61d05d82fd6abb9fbb110c562aac74a32
Jan Soendermann <jan.soendermann+git@gmail.com>
2018-11-24 10:45:45 +0800

    Implement a bunch of linked list methods

--- src/linked-list.test.ts
+++ src/linked-list.test.ts
@@ -1,9 +1,64 @@
 import LinkedList from './linked-list'
 
-describe('colorChineseString', () => {
-  test('simple', () => {
+describe('LinkedList', () => {
+  test('push', () => {
     const list = new LinkedList()
     list.push(42)
     expect(list.length).toEqual(1)
+  })
+
+  test('initialize with values', () => {
+    const list = new LinkedList([1, 2, 3])
+    expect(list.length).toEqual(3)
+  })
+
+  test('access elements', () => {
+    const list = new LinkedList([1, 2, 3])
+    expect(list.get(0)).toEqual(1)
+    expect(list.get(1)).toEqual(2)
+  })
+
+  test('remove', () => {
+    const list = new LinkedList([1, 2, 3, 4])
+    list.removeAt(0)
+    expect(list.get(0)).toEqual(2)
+    list.removeAt(1)
+    expect(list.get(1)).toEqual(4)
+  })
+
+  test('reverse', () => {
+    const list = new LinkedList([1, 2, 3])
+    list.reverse()
+    expect(list.toArray()).toEqual([3, 2, 1])
+  })
+
+  test('reverse empty list', () => {
+    const list = new LinkedList()
+    list.reverse()
+    expect(list.toArray()).toEqual([])
+  })
+
+  test('reverse list with one element', () => {
+    const list = new LinkedList([42])
+    list.reverse()
+    expect(list.toArray()).toEqual([42])
+  })
+
+  test('push array', () => {
+    const list = new LinkedList([1, 2])
+    list.pushArray([3, 4])
+    expect(list.toArray()).toEqual([1, 2, 3, 4])
+  })
+
+  test('push list', () => {
+    const list1 = new LinkedList([1, 2])
+    const list2 = new LinkedList([3, 4])
+    list1.pushList(list2)
+    expect(list1.toArray()).toEqual([1, 2, 3, 4])
+  })
+
+  test('get last element', () => {
+    const list = new LinkedList([1, 2, 3])
+    expect(list.getLast()).toEqual(3)
   })
 })
--- src/linked-list.ts
+++ src/linked-list.ts
@@ -6,6 +6,44 @@ interface ListNode<T> {
 export default class LinkedList<T> {
   private head: ListNode<T> | null = null
 
+  constructor(vals?: T[]) {
+    if (vals) {
+      for (const val of vals) {
+        this.push(val)
+      }
+    }
+  }
+
+  toString() {
+    return '[' + this.toArray().join(', ') + ']'
+  }
+
+  reverse() {
+    if (this.length === 0) {
+      return
+    }
+    let newHead = { val: this.head.val, next: null }
+    let p = this.head.next
+    while (p) {
+      newHead = { val: p.val, next: newHead }
+      p = p.next
+    }
+    this.head = newHead
+  }
+
+  toArray() {
+    if (!this.head) {
+      return []
+    }
+    const arr = []
+    let p = this.head
+    while (p) {
+      arr.push(p.val)
+      p = p.next
+    }
+    return arr
+  }
+
   push(val: T) {
     if (!this.head) {
       this.head = { val, next: null }
@@ -16,6 +54,64 @@ export default class LinkedList<T> {
       p = p.next
     }
     p.next = { val, next: null }
+  }
+
+  pushArray(vals: T[]) {
+    if (vals.length === 0) {
+      return
+    }
+    let arrIndex = 0
+    let lastElement
+    if (!this.head) {
+      this.head = { val: vals[arrIndex], next: null }
+      arrIndex++
+      lastElement = this.head
+    } else {
+      lastElement = this.head
+      while (lastElement.next) {
+        lastElement = lastElement.next
+      }
+    }
+    while (arrIndex < vals.length) {
+      lastElement.next = { val: vals[arrIndex], next: null }
+      lastElement = lastElement.next
+      arrIndex++
+    }
+  }
+
+  pushList(otherList: LinkedList<T>) {
+    if (!this.head) {
+      this.head = otherList.head
+      return
+    }
+    let lastElement = this.head
+    while (lastElement.next) {
+      lastElement = lastElement.next
+    }
+    lastElement.next = otherList.head
+  }
+
+  get(i: number) {
+    if (!this.head) {
+      throw new Error('List is empty')
+    }
+    let p = this.head
+    while (i > 0) {
+      p = p.next
+      i--
+    }
+    return p.val
+  }
+
+  getLast() {
+    if (!this.head) {
+      throw new Error('List is empty')
+    }
+    let lastElement = this.head
+    while (lastElement.next) {
+      lastElement = lastElement.next
+    }
+    return lastElement.val
   }
 
   get length() {
@@ -29,5 +125,21 @@ export default class LinkedList<T> {
       l++
     }
     return l
+  }
+
+  removeAt(i: number) {
+    if (!this.head) {
+      throw new Error('List is empty')
+    }
+    if (i === 0) {
+      this.head = this.head.next
+      return
+    }
+    let p = this.head
+    while (i > 1) {
+      p = p.next
+      i--
+    }
+    p.next = p.next.next
   }
 }
