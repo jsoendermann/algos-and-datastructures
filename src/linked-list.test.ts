f73c830d51d389e0499c08efb680fa5ee17aa0eb
Jan Soendermann <jan.soendermann+git@gmail.com>
2018-11-24 10:05:25 +0800

    First list test

---
+++ src/linked-list.test.ts
@@ -0,0 +1,9 @@
+import LinkedList from './linked-list'
+
+describe('colorChineseString', () => {
+  test('simple', () => {
+    const list = new LinkedList()
+    list.push(42)
+    expect(list.length).toEqual(1)
+  })
+})
