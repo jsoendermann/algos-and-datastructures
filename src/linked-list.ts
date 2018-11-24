0f8d99b56ce81bae638612eb7f574e9a652e8c6a
Jan Soendermann <jan.soendermann+git@gmail.com>
2018-11-24 10:10:38 +0800

    Make test pass

--- src/linked-list.ts
+++ src/linked-list.ts
@@ -1,1 +1,33 @@
-export default class LinkedList {}
+interface ListNode<T> {
+  val: T
+  next: ListNode<T> | null
+}
+
+export default class LinkedList<T> {
+  private head: ListNode<T> | null = null
+
+  push(val: T) {
+    if (!this.head) {
+      this.head = { val, next: null }
+      return
+    }
+    let p = this.head
+    while (p.next !== null) {
+      p = p.next
+    }
+    p.next = { val, next: null }
+  }
+
+  get length() {
+    if (!this.head) {
+      return 0
+    }
+    let l = 1
+    let p = this.head
+    while (p.next !== null) {
+      p = p.next
+      l++
+    }
+    return l
+  }
+}
